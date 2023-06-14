042dfd36cb3684962671f045256480f4fe713b73
DeguzBelarus <deguz@mail.ru>
2023-06-14 16:58:19 +0300

    feat: solve the cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,25 @@
+import { spawn } from 'node:child_process';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const script = spawn('node', [path.join(__dirname, 'files', 'script.js'), ...args]);
+
+  script.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+    script.stdin.write('CLOSE');
+  });
+
+  script.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  script.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['hello', 'world!']);
