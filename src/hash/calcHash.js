bd8d357a63e14c1e842031d152d1dfb3f159299a
DeguzBelarus <deguz@mail.ru>
2023-06-14 23:24:11 +0300

    feat: solve the hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,37 @@
+import fs from 'fs';
+import path, {
+  dirname
+} from 'path';
+import {
+  fileURLToPath
+} from 'url';
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(
+    import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const algorithm = 'sha256';
+  const fileToEncryptPath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+  fs.access(fileToEncryptPath, (error) => {
+    if (error) {
+      throw new Error("The fileToCalculateHashFor.txt file was not found");
+    } else {
+      fs.readFile(fileToEncryptPath, (error, data) => {
+        if (error) {
+          console.error(error.message);
+        }
+        if (data) {
+          const hashSum = crypto.createHash(algorithm);
+          hashSum.update(data);
+          const hex = hashSum.digest('hex');
+          console.log('hash: ', hex);
+        }
+      })
+    }
+  })
 };
 
 await calculateHash();
