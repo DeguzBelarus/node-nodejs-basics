11b1163734b213fb1f42b29df227b5160a58f992
DeguzBelarus <deguz@mail.ru>
2023-06-14 22:58:45 +0300

    feat: solve the fs read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,34 @@
+import fs from 'fs';
+import path, {
+  dirname
+} from 'path';
+import {
+  fileURLToPath
+} from 'url';
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(
+    import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const fileToReadPath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+  fs.access(fileToReadPath, (error) => {
+    if (error) {
+      throw new Error('FS operation failed');
+    } else {
+      fs.readFile(fileToReadPath, {
+        encoding: 'utf-8'
+      }, (error, data) => {
+        if (error) {
+          console.error(error.message);
+        }
+        if (data) {
+          console.log(data);
+        }
+      })
+    }
+  })
 };
 
 await read();
