0082ef9e8a40102798467d0ba62d2dc395dc8a1f
DeguzBelarus <deguz@mail.ru>
2023-06-14 22:52:28 +0300

    feat: solve the fs list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,36 @@
+import fs from 'fs';
+import path, {
+  dirname
+} from 'path';
+import {
+  fileURLToPath
+} from 'url';
+import {
+  readdir
+} from "fs/promises";
+
 const list = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(
+    import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const filesDirPath = path.join(__dirname, 'files');
+
+  fs.access(filesDirPath, async (error) => {
+    if (error) {
+      throw new Error('FS operation failed');
+    } else {
+      const files = await readdir(filesDirPath, {
+        withFileTypes: true,
+      });
+
+      if (files.length) {
+        console.log(files.map((file) => file.name));
+      } else {
+        console.log('The files folder is empty');
+      }
+    }
+  })
 };
 
 await list();
