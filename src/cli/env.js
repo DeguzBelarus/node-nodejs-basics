c9247e8435dbe0acf4892db500032ca1de0d53a9
DeguzBelarus <deguz@mail.ru>
2023-06-14 14:44:04 +0300

    feat: solve the cli env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVariables = Object
+    .entries(process.env)
+    .filter((envVariable) => envVariable[0].startsWith('RSS_'));
+  if (envVariables.length) {
+    let result = '';
+    for (let i = 0; i < envVariables.length; i++) {
+      i < envVariables.length - 1 ?
+        result += `${envVariables[i][0]}=${envVariables[i][1]}; ` :
+        result += `${envVariables[i][0]}=${envVariables[i][1]}`;
+    }
+    console.log(result);
+  } else {
+    console.log('there are no environment variables')
+  }
 };
 
 parseEnv();
