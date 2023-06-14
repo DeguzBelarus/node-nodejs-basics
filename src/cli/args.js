ae00fbeaf4493725f2511f53f12311d7b2d44a2d
DeguzBelarus <deguz@mail.ru>
2023-06-14 14:10:43 +0300

    feat: solve the cli args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argumentsArray = process.argv.slice(2);
+  if (argumentsArray.length) {
+    let result = '';
+    for (let i = 0; i < argumentsArray.length; i += 2) {
+      i < argumentsArray.length - 2 ?
+        result += `${argumentsArray[i].slice(2)} is ${argumentsArray[i + 1]}, ` :
+        result += `${argumentsArray[i].slice(2)} is ${argumentsArray[i + 1]}`;
+    }
+    console.log(result);
+  } else {
+    console.log('there are no arguments')
+  }
 };
 
 parseArgs();
