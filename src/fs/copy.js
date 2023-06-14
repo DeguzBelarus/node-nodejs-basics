22d5bb4a004467fb3c58fced1773a616fefe43b0
DeguzBelarus <deguz@mail.ru>
2023-06-14 22:33:56 +0300

    feat/fix: solve the fs rename task and fix the fs copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -17,45 +17,46 @@ const copy = async () => {
   const filesDirPath = path.join(__dirname, 'files');
   const filesCopyDirPath = path.join(__dirname, 'files_copy');
 
-  fs.access(filesDirPath, error => {
+  fs.access(filesDirPath, (error) => {
     if (error) {
-      throw new Error('FS operation failed ');
-    }
-  })
-  fs.access(filesCopyDirPath, error => {
-    if (!error) {
-      throw new Error('FS operation failed ');
-    }
-  })
-
-  fs.mkdir(filesCopyDirPath, {
-    recursive: true
-  }, async (error) => {
-    console.log("Creating a folder files_copy...");
-    if (error) {
-      console.error(error.message);
+      throw new Error('FS operation failed');
     } else {
-      console.log("The files_copy folder has been created!");
-
-      const files = await readdir(filesDirPath, {
-        withFileTypes: true,
-      });
-
-      if (files.length) {
-        for (const file of files) {
-          console.log(`Copying the ${file.name} file...`);
-          fs.copyFile(path.join(filesDirPath, file.name), path.join(filesCopyDirPath, file.name), (error) => {
+      fs.access(filesCopyDirPath, (error) => {
+        if (!error) {
+          throw new Error('FS operation failed');
+        } else {
+          fs.mkdir(filesCopyDirPath, {
+            recursive: true
+          }, async (error) => {
+            console.log("Creating a folder files_copy...");
             if (error) {
               console.error(error.message);
+            } else {
+              console.log("The files_copy folder has been created!");
+
+              const files = await readdir(filesDirPath, {
+                withFileTypes: true,
+              });
+
+              if (files.length) {
+                for (const file of files) {
+                  console.log(`Copying the ${file.name} file...`);
+                  fs.copyFile(path.join(filesDirPath, file.name), path.join(filesCopyDirPath, file.name), (error) => {
+                    if (error) {
+                      console.error(error.message);
+                    }
+                  })
+                }
+                console.log('All files have been copied!');
+              } else {
+                console.log('The files folder is empty');
+              }
             }
-          })
+          });
         }
-        console.log('All files have been copied!');
-      } else {
-        console.log('The files folder is empty');
-      }
+      })
     }
-  });
+  })
 };
 
 await copy();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,38 @@
+import fs from 'fs';
+import path, {
+  dirname
+} from 'path';
+import {
+  fileURLToPath
+} from 'url';
+
 const rename = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(
+    import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const wrongFilepath = path.join(__dirname, 'files', 'wrongFilename.txt');
+  const properFilepath = path.join(__dirname, 'files', 'properFilename.txt');
+
+  fs.access(wrongFilepath, (error) => {
+    if (error) {
+      throw new Error('FS operation failed');
+    } else {
+      fs.access(properFilepath, (error) => {
+        if (!error) {
+          throw new Error('FS operation failed');
+        } else {
+          fs.rename(wrongFilepath, properFilepath, (error) => {
+            if (error) {
+              console.error(error.message);
+            } else {
+              console.log('The wrongFilename.txt was successfully renamed to properFilename.txt!');
+            }
+          })
+        }
+      })
+    }
+  })
 };
 
 await rename();
