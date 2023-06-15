74dce2de866277fe21a20879630fa705181a1a5c
DeguzBelarus <deguz@mail.ru>
2023-06-15 18:00:00 +0300

    fix: fix the streams transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,41 +1,25 @@
 import {
-  Transform
+  Transform,
+  pipeline
 } from 'stream';
-import {
-  ReadStream,
-  WriteStream,
-} from 'fs';
-import {
-  dirname,
-  join,
-} from 'path';
-import {
-  fileURLToPath
-} from 'url';
 
 const transform = async () => {
-  const __filename = fileURLToPath(
-    import.meta.url);
-  const __dirname = dirname(__filename);
+  const readableStream = process.stdin;
+  const writeableStream = process.stdout;
 
-  const fileToReadPath = join(__dirname, 'files', 'fileToRead.txt')
-  const fileToWritePath = join(__dirname, 'files', 'fileToWrite.txt');
-  const readStream = new ReadStream(fileToReadPath);
-  const writeStream = new WriteStream(fileToWritePath);
   const transformStream = new Transform({
-    transform(data, encoding, callback) {
-      encoding = 'utf8';
-      callback(null, data.toString().split('').reverse().join(''));
+    transform(chunk, encoding, callback) {
+      chunk.toString().trim() === 'exit' && process.exit(0);
+      console.log('Enter your text (or type "exit"): ');
+      this.push(chunk.toString().trim().split('').reverse().join('') + '\n');
+      callback();
     },
   });
-  transformStream.on('finish', () => {
-    console.log('The text from the fileToRead.txt file was reversed');
-  })
-  writeStream.on('finish', () => {
-    console.log('The fileToWrite.txt file was updated with the reversed text!');
-  })
 
-  readStream.pipe(transformStream).pipe(writeStream);
+  console.log('Enter your text (or type "exit"): ');
+  pipeline(readableStream, transformStream, writeableStream, (error) => {
+    console.error(error);
+  })
 };
 
 await transform();
